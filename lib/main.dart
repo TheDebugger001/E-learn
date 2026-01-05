f72e7280559047ef7d8caad16da0ce94105af909
TheDebugger001 <thedebugger001@gmail.com>
2026-01-05 17:51:52 +0200

    This is the basics of layout and styling

--- lib/main.dart
+++ lib/main.dart
@@ -18,114 +18,191 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         body: Column(
           children: [
-            TopNavbar(),
-            Expanded(
-              child: Center(
-                child: Text("Other content here"),
-              ))
+            Column(
+              children: [
+                SizedBox(height: 150,),
+                Container(
+                  height: 100,
+                  width: 450,
+                  color: Colors.red,
+                  child: const Center(
+                    child: Text(
+                      "HELLO WORLD"
+                    ),
+                  )
+                )
+              ],
+            ),
+
+            Column(
+              children: [
+                SizedBox(height: 100,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 200,
+                      color: Colors.blue,
+                      child: const Center(
+                        child: Text("Eric"),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 200,
+                      color: Colors.blue,
+                      child: const Center(
+                        child: Text("James"),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+
+            Column(
+              children: [
+                SizedBox(height: 100,),
+                
+                Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(  //! This is the whole columns in part 1
+                      children: [
+                        Column(  //! This is the whole row of the bottom part 1 top
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Container(
+                                          height: 60,
+                                          width: 100,
+                                          color: Colors.orange,
+                                          child: const Center(
+                                            child: Text("Me"),
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 60,
+                                          width: 100,
+                                          color: Colors.orange,
+                                          child: const Center(
+                                            child: Text("You"),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+
+                              ],
+                            ),
+                          ],
+                        ),
+
+                        Column(  //! This is the whole row of the bottom part 1 bottom
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Container(
+                                          height: 60,
+                                          width: 100,
+                                          color: Colors.grey,
+                                          child: const Center(
+                                            child: Text("Me"),
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 60,
+                                          width: 100,
+                                          color: Colors.grey,
+                                          child: const Center(
+                                            child: Text("You"),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Column(  //! This is the whole columns in part 1
+                      children: [
+                        Column(  //! This is the whole row of the bottom part 2 top
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Container(
+                                          height: 60,
+                                          width: 200,
+                                          color: Colors.purple,
+                                          child: const Center(
+                                            child: Text("Me"),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+
+                              ],
+                            ),
+                          ],
+                        ),
+
+                        Column(  //! This is the whole row of the bottom part 2 bottom
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Container(
+                                          height: 60,
+                                          width: 200,
+                                          color: Colors.purple,
+                                          child: const Center(
+                                            child: Text("Me"),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )
           ],
         ),
-      ),
+        )
     );
   }
 }
 
-class  TopNavbar extends StatelessWidget{
-  const TopNavbar({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 4,
-            offset: Offset(0, 4)
-          )
-        ]
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            "My Todo",
-            style: TextStyle(color: Colors.white, fontSize: 20)
-            ),
-          Row(
-            children: [
-              Icon(Icons.search, color: Colors.white, size: 20,),
-              SizedBox(width: 20),
-              Icon(Icons.notifications, color: Colors.white),
-              SizedBox(width: 20,),
-              Icon(Icons.person, color: Colors.white,)
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-        
-//       ),
-//       body: Center(
-//         child: Text(
-//           "$count",
-//           style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-//         ),
-//       ),
-
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: increment,
-//         child: const Icon(Icons.add),
-//         ),
-//     );
-//   }
-
-// }
-
-
-
-
-
-
-
-
-// ? This is the hello world in flutter
-// Scaffold(
-//         appBar: AppBar(
-//           title: Text("INJIRA BORA"),
-//           backgroundColor: Colors.black12,
-//         ),
-//         body: const Center(
-//           child: Text(
-//             "Hello, Guerschom",
-//             style: TextStyle(fontSize:30),
-//           ),
-//         ),
-//       ),
-
-
-// class CounterPage extends StatefulWidget{
-//   const CounterPage({super.key});
-
-//   @override
-//   State<CounterPage> createState() => _CounterPageState();
-// }
-
-// class _CounterPageState extends State<CounterPage>{
-//   int count = 0;
-
-//   void increment() {
-//     setState(() {
-//       count += 1;
-//     });
-//   }
