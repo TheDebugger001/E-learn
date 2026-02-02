73137533b3b2728bf22e5ef82d0bb019bfbc3187
TheDebugger001 <thedebugger001@gmail.com>
2026-02-02 15:37:15 +0200

    End of the upper design

--- lib/components/tour/profile.dart
+++ lib/components/tour/profile.dart
@@ -9,7 +9,30 @@ class Profile extends StatelessWidget {
       clipBehavior: Clip.none,
       children: [
         // Background container
-        Container(width: double.infinity, height: 70, color: Colors.blue),
+        Container(
+          width: double.infinity,
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  "Profile",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: Icon(Icons.edit, color: Colors.white, size: 30),
+              ),
+            ],
+          ),
+        ),
 
         // Overlapping container
         Positioned(
@@ -69,16 +92,66 @@ class Profile extends StatelessWidget {
                           fontWeight: FontWeight.w900,
                         ),
                       ),
-                      Text("rengokusama@gmail.com", style: TextStyle(fontSize: 13,color: const Color.fromARGB(189, 82, 81, 81)),),
+                      Text(
+                        "rengokusama@gmail.com",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: const Color.fromARGB(189, 82, 81, 81),
+                        ),
+                      ),
                     ],
                   ),
                 ),
 
                 Container(
-                  width: 500,
+                  width: 350,
                   height: 80,
-                  color: Colors.orange,
-                )
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(85, 132, 131, 130),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: ClipRRect(
+                            child: Image.asset("assets/images/medal0.png"),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: ClipRRect(
+                            child: Image.asset("assets/images/medal2.png"),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: ClipRRect(
+                            child: Image.asset("assets/images/medal3.png"),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: ClipRRect(
+                            child: Image.asset("assets/images/medal4.png"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
